5ada1fff9dfa55936c1efa890e1df37a25f92a79
Jenis <jeniswinsta@gmail.com>
2024-03-08 17:02:43 +0530

    unknown

--- users/admin.py
+++ users/admin.py
@@ -13,7 +13,7 @@ class CustomUserAdmin(UserAdmin):
 
     model = CustomUser
 
-    list_display = ('username', 'email', 'role', 'department', 'is_active',
+    list_display = ('username', 'email', 'role', 'department', 'user_type', 'is_active',
                     'is_staff', 'is_superuser', 'last_login',)
     list_filter = ('is_active', 'is_staff', 'is_superuser')
     fieldsets = (
--- users/models.py
+++ users/models.py
@@ -11,6 +11,9 @@ class CustomUser(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     role = models.CharField(_('role'), max_length=100, blank=True, null=True)
     department = models.CharField(_('department'), max_length=100, blank=True, null=True)
+
+    user_type = models.CharField(max_length=20, choices=[('admin', 'Admin'), ('teacher', 'Teacher'), ('hod', 'HOD')], default='teacher')
+
     
 
     USERNAME_FIELD = 'username'
