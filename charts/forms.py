a8f263fa949ace00d336576743cab0a8d5819081
MagnusNazareth <nazarethmagnus@gmail.com>
2024-03-30 16:59:39 +0530

    whole project

--- charts/forms.py
+++ charts/forms.py
@@ -1,5 +1,15 @@
+# forms.py
 from django import forms
-from .models import Student
+from .models import *
+from django.core.exceptions import ValidationError
 
-class UploadFileForm(forms.Form):
-    file= forms.FileField(label='Select an Excel file')
+
+class ExcelUploadForm(forms.Form):
+    file = forms.FileField(label='Select an Excel file')
+    year = forms.ModelChoiceField(queryset=File_Description.objects.all())
+
+    def clean_file(self):
+        file = self.cleaned_data['file']
+        if not file.name.endswith('.xlsx'):
+            raise ValidationError('Only .xlsx files are allowed.')
+        return file                                                                                            
