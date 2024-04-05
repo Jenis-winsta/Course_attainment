fd0cf006d480ef08b97e094ce63418d68b0312a5
MagnusNazareth <nazarethmagnus@gmail.com>
2024-04-06 04:34:09 +0530

    changes

--- charts/admin.py
+++ charts/admin.py
@@ -7,11 +7,7 @@ from .forms import *
 @admin.register(Student)
 class StudentAdmin(ImportExportModelAdmin):
     
-<<<<<<< HEAD
-    list_display = ( 'file_desc', 'name', 'cia_marks', 'semester_marks', 'total_marks',)
-=======
     list_display = ('name', 'cia_marks', 'semester_marks', 'total_marks',)
->>>>>>> error
     
 
 
--- charts/models.py
+++ charts/models.py
@@ -4,19 +4,6 @@ from techapp.models import *
 
 
 class File_Description(models.Model):
-<<<<<<< HEAD
-    year = models.CharField(max_length=10)    
-    sheet_name = models.CharField(max_length=100, blank=True, null=True)
-
-    def __str__(self):
-        if self.sheet_name:
-            return f"{self.year} - {self.sheet_name}"
-        else:
-            return str(self.year)
-
-class Student(models.Model):
-    name = models.CharField(max_length=250)
-=======
     year_of_file = models.CharField(max_length=20)        
     course = models.ForeignKey(Course, on_delete=models.CASCADE)     # techapp course models
 
@@ -26,7 +13,6 @@ class Student(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=250) 
->>>>>>> error
     cia_marks = models.IntegerField()
     semester_marks = models.IntegerField()
     total_marks = models.IntegerField()  
