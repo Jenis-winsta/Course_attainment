fff505aa43c67a0e386bf04487a8d6accda1c1d2
MagnusNazareth <nazarethmagnus@gmail.com>
2024-04-24 17:26:33 +0530

    change of db from sqlite to postgresql

--- Course_attainment/settings.py
+++ Course_attainment/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+# Key encryption
+# import os
+from decouple import config
 from pathlib import Path
 
 
@@ -21,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jawvcb*ct+t!zq^8k#oe^_(%s6wy)o*v18hh*5_rokua&u+_yt'
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY='django-insecure-jawvcb*ct+t!zq^8k#oe^_(%s6wy)o*v18hh*5_rokua&u+_yt'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = config('DEBUG', cast=bool)
 DEBUG = False
 CSRF_USE_SESSIONS = False
 
@@ -36,8 +41,8 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 AUTH_USER_MODEL = 'users.CustomUser'
 
 
-# Pandas Modin
-# import os
+
+
 # # Modin will use Dask
 # os.environ["MODIN_ENGINE"] = "dask"  
 
@@ -108,16 +113,39 @@ WSGI_APPLICATION = 'Course_attainment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         #database name
+#         'NAME': config('DB_NAME'),         
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': '',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        #database name
+        'NAME': 'cams',
+        'USER': 'postgres',
+        'PASSWORD': 'Postgresql@1521',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+# https://docs.djangoprojecxt.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
